ed4d31c0bd824d1d2b8c55ffd2fdb8b59206531e
Hugo Sodré <hugosodre031@gmail.com>
2023-05-23 10:25:08 -0300

    Foto de perfil atualizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary" :'#0a192f',
-        "secundary": '#64ffda',
-        "third": '#CCD6F6'
-      }
+        primary: "#0a192f",
+        secundary: "#FFE071",
+        third: "#CCD6F6",
+        "background-color": "#0C151D",
+      },
     },
   },
   plugins: [],
-}
+};
