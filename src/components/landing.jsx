eddc0d8f553d49c737b6e3b7719578c39eec088b
Hugo Sodré <hugosodre031@gmail.com>
2023-05-23 21:16:16 -0300

    commit 2

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,5 +1,7 @@
 import Perfil from "../assets/perfil.jpg";
-import Teste from "../assets/career.svg";
+import Git from "../assets/github.svg";
+import Linkedas from "../assets/linkedin.svg";
+import Twitter from "../assets/tt.svg";
 function Landing() {
   return (
     <section className="font-bold h-screen flex flex-col items-center justify-center">
@@ -12,6 +14,25 @@ function Landing() {
               alt="Pefil"
             />
           </div>
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl text-white p-2">Hugo Silva Sodré</h1>
+          <p className="text-[#A3ABB2]">Software Engineer</p>
+        </div>
+
+        <div className="flex flex-row items-center justify-center gap-6">
+          <a href="https://github.com/Hugo-Sodre " target="_blank">
+            <img src={Git} alt="github image" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/hugo-silva-sodr%C3%A9-4495b3207/"
+            target="_blank"
+          >
+            <img src={Linkedas} alt="Linkedin image" />
+          </a>
+          <a href="#">
+            <img src={Twitter} alt="Twitter image" target="_blank" />
+          </a>
         </div>
       </div>
     </section>
